5a901e8002468db799cedb128bdf99488d70c2a0
joelkim <kim.dohhyoung@gmail.com>
2017-06-28 12:42:42 +0900

    fix error in docker-entrypoint.sh and add README

--- .docker-entrypoint.sh
+++ .docker-entrypoint.sh
@@ -7,7 +7,7 @@ service postgresql start
 
 # postgres setup for django
 sudo -u postgres psql postgres -c "create role admin with password 'adminpass'"
-sudo -u postgres psql postgres -f "/home/$USER_ID/..postgres_db_setup.sql"
+sudo -u postgres psql postgres -f "/home/$USER_ID/.postgres_db_setup.sql"
 
 /bin/bash
 
